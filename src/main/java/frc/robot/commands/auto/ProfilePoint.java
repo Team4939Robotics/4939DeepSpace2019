1faf6195852fa99c8f22a748d6a0dfcdda0b6e7e
Nicole <36340428+Team4939Robotics@users.noreply.github.com>
2019-01-10 15:54:24 -0500

    Create Profit Point
    Removed time property

---
+++ src/main/java/frc/robot/commands/auto/ProfilePoint.java
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.auto;
+
+/**
+ * Add your docs here.
+ */
+public class ProfilePoint {
+
+	private double pos; // position in inches
+	private double vel; // velocity in feet/s
+	private double acc; // acc in ft/s^2
+	
+
+	// trajectory point with pos, vel, acc, time and intake boolean
+	public ProfilePoint(double pos, double vel, double acc, double time) {
+		this.pos = pos;
+		this.vel = vel;
+		this.acc = acc;
+
+	}
+
+	public double getPos() {
+		return pos;
+	}
+
+	public double getVel() {
+		return vel;
+	}
+
+	public double getAcc() {
+		return acc;
+	}
+}

39e3b889a75ad787bf8eb7b3d973f4d0a5b881b5
Meshva <meshva03@gmail.com>
2019-01-11 16:17:30 -0500

    Update FollowProfile.java
    Follows the profile

--- src/main/java/frc/robot/commands/FollowProfile.java
+++ src/main/java/frc/robot/commands/FollowProfile.java
@@ -1,16 +1,12 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import com.frc.robot;
+import frc.ProfilePoint;
 
 public class FollowProfile extends Command {
-  private Trajectory trajectory;
+  private ProfilePoint profile;
+  
   public FollowProfile() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
@@ -25,6 +21,12 @@ public class FollowProfile extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double position = profile.getPos(); //might need to reference index
+    double velocity = profile.getVel();
+    double acceleration = profile.getAcc();
+
+    double currentLeftPos = Robot.DriveBase.getLeftEncoderDist;
+    double currentRightPos = Robot.DriveBase.getRightEncoderDist;
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -44,3 +46,4 @@ public class FollowProfile extends Command {
   protected void interrupted() {
   }
 }
+

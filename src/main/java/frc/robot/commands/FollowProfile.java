673ce3890984bfb7d8886ab006ef4bf3df301a9a
Meshva <meshva03@gmail.com>
2019-01-16 15:18:20 -0500

    Update FollowProfile.java

--- src/main/java/frc/robot/commands/FollowProfile.java
+++ src/main/java/frc/robot/commands/FollowProfile.java
@@ -8,6 +8,7 @@ public class FollowProfile extends Command {
   private ProfilePoint profile;
   
   public FollowProfile() {
+    requires(Robot.dt);
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
@@ -15,7 +16,7 @@ public class FollowProfile extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    Robot.DriveBase.resetEncoders();
+    Robot.dt.resetEncoders();
   }
 
   // Called repeatedly when this Command is scheduled to run
@@ -25,19 +26,27 @@ public class FollowProfile extends Command {
     double velocity = profile.getVel();
     double acceleration = profile.getAcc();
 
-    double currentLeftPos = Robot.DriveBase.getLeftEncoderDist;
-    double currentRightPos = Robot.DriveBase.getRightEncoderDist;
+    double currentLeftPos = Robot.dt.getLeftEncoderDist;
+    double currentRightPos = Robot.dt.getRightEncoderDist;
+
+    Robot.dt.runLeftDrive(ProfileController.output(leftPos, leftVel, leftAcc, currentLeftPos));
+    Robot.dt.runRightDrive(ProfileController.output(rightPos, rightVel, rightAcc, currentRightPos));
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+   if (index < profile.getProfile().length)
+        	return false;
+        else
+        	return true;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.dt.runLeftSideDrive(0);
+    Robot.dt.runRightSideDrive(0);
   }
 
   // Called when another command which requires one or more of the same
